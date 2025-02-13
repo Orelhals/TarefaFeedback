b6963e7d7ec09f30f9e2b4f7e66fa2af64ce49f8
Arthur Pimenta <arthurstt22@gmail.com>
2025-02-13 17:38:01 -0300

    feat: Finalizando e adicionando o feedback responsivo

--- js/index.js
+++ js/index.js
@@ -6,16 +6,23 @@ function enviaDados() {
   const nomeElement = document.getElementById("feedback-nome");
   const feedback = feedbackElement.value;
   const nome = nomeElement.value;
-  console.log(feedback);
-  console.log(nome);
 
-  // Criar uma li com os dados e enviar para dentro da ul que vai aparecer na tela
-  const ul = document.getElementById("feed-list");
-  const li = document.createElement("li");
-  li.innerHTML = `${nome}: ${feedback}`;
-  ul.appendChild(li);
+  if (feedback === "") {
+    alert("Você se esqueceu de deixar seu feedback");
+  } else if (nome === "") {
+    alert("Você esqueceu de informar seu nome");
+  } else {
+    // Criar uma li com os dados e enviar para dentro da ul que vai aparecer na tela
+    const ul = document.getElementById("feed-list");
+    const h2feed = document.getElementById("h2-feed");
+    const li = document.createElement("li");
+    const h2 = document.createElement("h2");
+    li.innerHTML = `${nome}: ${feedback}`;
+    h2feed.innerHTML = "Comentários:";
+    ul.appendChild(li);
 
-  // Limpar os campos de feedback e nome
-  feedbackElement.value = "";
-  nomeElement.value = "";
+    // Limpar os campos de feedback e nome
+    feedbackElement.value = "";
+    nomeElement.value = "";
+  }
 }
